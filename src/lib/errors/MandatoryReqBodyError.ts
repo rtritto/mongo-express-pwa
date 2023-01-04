a4c6093a486e350f3ab7b1ca307025e43eb85e11
rtritto <r.tritto5@studenti.uniba.it>
2023-01-04 03:33:22 +0100

    Lint

--- src/lib/errors/MandatoryReqBodyError.ts
+++ src/lib/errors/MandatoryReqBodyError.ts
@@ -1,7 +1,7 @@
 export default class MandatoryReqBodyError extends Error {
   status: number
 
-  constructor(message: string = 'Missing body request') {
+  constructor(message = 'Missing body request') {
     super(message)
     this.name = 'MandatoryReqBodyError'
     this.status = 400
